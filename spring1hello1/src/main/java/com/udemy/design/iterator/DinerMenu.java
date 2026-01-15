1e51644a2d008887c2ce68c91028743a7d74319d
reo0306 <reo19840306@gmail.com>
2026-01-15 23:55:43 +0900

    add iterator

---
+++ spring1hello1/src/main/java/com/udemy/design/iterator/DinerMenu.java
@@ -0,0 +1,30 @@
+package com.udemy.design.iterator;
+
+public class DinerMenu {
+    static final int MAX_ITEMS = 6;
+    int numberOfItems = 0;
+    MenuItem[] menuItems;
+
+    public DinerMenu() {
+        menuItems = new MenuItem[MAX_ITEMS];
+
+        addItem("ベジタリアンBLT", "サンドイッチ1", true, 2.99);
+        addItem("BLT", "サンドイッチ2", false, 2.99);
+        addItem("本日のスープ", "ポテトサラダを添えた本日のスープ", false, 3.29);
+        addItem("Hotdog", "ホットドッグ", false, 3.05);
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price) {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        if (numberOfItems >= MAX_ITEMS) {
+            System.out.println("メニューはいっぱいです！メニューに項目を追加できません");
+        } else {
+            menuItems[numberOfItems] = menuItem;
+            numberOfItems = numberOfItems + 1;
+        }
+    }
+
+    public MenuItem[] getMenuItems() {
+        return menuItems;
+    }
+}
---
+++ spring1hello1/src/main/java/com/udemy/design/iterator/PancakeHouseMenu.java
@@ -0,0 +1,25 @@
+package com.udemy.design.iterator;
+
+import java.util.ArrayList;
+
+public class PancakeHouseMenu {
+    ArrayList<MenuItem> menuItems;
+
+    public PancakeHouseMenu() {
+        menuItems = new ArrayList<MenuItem>();
+
+        addItem("K&Bのパンケーキ朝食", "スクランブルエッグとトースト付きパンケーキ", true, 2.99);
+        addItem("いつものパンケーキ朝食", "卵焼きとソーセージ付きパンケーキ", false, 2.99);
+        addItem("ブルーベリーパンケーキ", "新鮮なブルーベリーを使ったパンケーキ", true, 3.49);
+        addItem("ワッフル", "ブルーベリーか苺の好きなほうをのせたパンケーキ", true, 3.59);
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price) {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        menuItems.add(menuItem);
+    }
+
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+}
